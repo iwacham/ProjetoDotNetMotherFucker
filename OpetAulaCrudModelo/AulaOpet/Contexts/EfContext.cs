56543ec56984e7dc0159fafca3b54761e650754b
Tiago Henrique Iwamoto <ti.breakout@gmail.com>
2017-05-24 22:07:32 -0300

    Add files via upload

---
+++ OpetAulaCrudModelo/AulaOpet/Contexts/EfContext.cs
@@ -0,0 +1,30 @@
+﻿using AulaOpet.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AulaOpet.Contexts
+{
+    public class EfContext : DbContext
+    {
+
+        #region [Construtor]
+        public EfContext() : base("MEUCRUD")
+        {
+            Database.SetInitializer<EfContext>(
+               new DropCreateDatabaseIfModelChanges<EfContext>());
+        }
+        #endregion
+
+        #region [DbSet's]
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        #endregion
+
+       
+    }
+}
